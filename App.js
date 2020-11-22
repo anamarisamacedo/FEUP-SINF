40f9a3e40f2425ed0553f993b9c39beb7f388dfc
Sérgio Dias <47751859+SergioDias47@users.noreply.github.com>
2020-11-22 16:24:30 +0000

    //WIP: react-navigation setup

--- App.js
+++ App.js
@@ -1,36 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
 import WarehouseButton from './components/WarehouseButton';
 
-export default function App() {
-  return (
-    <View style={styles.main}>
-      <Text>I know it's just the beginning, but the project is now created!</Text>
-      <StatusBar style="auto" />
-      <View style={styles.warehouseBtns}>
-        <View style={styles.row}>
-          <WarehouseButton id="A1" name="CPU" />
-          <WarehouseButton id="B1" name="HDD" />
-          <WarehouseButton id="C1" name="Case" />
-        </View>
-        <View style={styles.row}>
-          <WarehouseButton id="A2" name="GPU" />
-          <WarehouseButton id="B2" name="SSD" />
-          <WarehouseButton id="C2" name="Cooler" />
-        </View>
-        <View style={styles.row}>
-          <WarehouseButton id="A3" name="MB" />
-          <WarehouseButton id="B3" name="RAM" />
-          <WarehouseButton id="C3" name="PSU" />
-        </View>
-        <View style={[styles.row, {marginTop: 50}]}>
-          <WarehouseButton id="OP" name="" />
+class HomeScreen extends React.Component {
+  render() {
+    const { nagivate } = this.props.nagivation;
+    return (
+      <View style={styles.main}>
+        <Text>I know it's just the beginning, but the project is now created!</Text>
+        <StatusBar style="auto" />
+        <View style={styles.warehouseBtns}>
+          <View style={styles.row}>
+            <WarehouseButton id="A1" name="CPU" />
+            <WarehouseButton id="B1" name="HDD" />
+            <WarehouseButton id="C1" name="Case" />
+          </View>
+          <View style={styles.row}>
+            <WarehouseButton id="A2" name="GPU" />
+            <WarehouseButton id="B2" name="SSD" />
+            <WarehouseButton id="C2" name="Cooler" />
+          </View>
+          <View style={styles.row}>
+            <WarehouseButton id="A3" name="MB" />
+            <WarehouseButton id="B3" name="RAM" />
+            <WarehouseButton id="C3" name="PSU" />
+          </View>
+          <View style={[styles.row, { marginTop: 50 }]}>
+            <WarehouseButton id="OP" name="" />
+          </View>
         </View>
       </View>
-    </View>
-  );
+    );
+
+  }
+}
+
+const NavigationApp = StackNavigator({
+  Home: { screen: HomeScreen },
+}, {
+  nagivationOptions: {
+    headerStyle: {
+      
+    }
+  }
+}
+);
+
+export default class App extends React.Component {
+  render() {
+    return <NavigationApp />
+  }
 }
 
 const styles = StyleSheet.create({
@@ -51,3 +73,4 @@ const styles = StyleSheet.create({
     marginTop: 180
   }
 });
+
