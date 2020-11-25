2ecc5f810d15b7753a30ec63225261ca2204d1b6
leonormsousa <leonormsousa@sapo.pt>
2020-11-25 20:20:53 +0000

    rebase and advanced what i could in navbar

--- App.js
+++ App.js
@@ -2,91 +2,10 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import WarehouseScreen from './screens/WarehouseScreen';
+import NavigationDrawerStructure from './components/NavigationDrawerStructure';
 //import Navigator from './HomeStack';
 
 const Drawer = createDrawerNavigator();
-
-const NavigationDrawerStructure = (props) => {
-  //Structure for the navigatin Drawer
-  const toggleDrawer = () => {
-    //Props to open/close the drawer
-    props.navigationProps.toggleDrawer();
-  };
-
-  return (
-    <View style={{ alignItems: 'left', justifyContent: 'center', backgroundColor: '#E5E5E5'}}>
-        <TouchableHighlight onPress={() => navigation.navigate('WarehouseScreen')}>
-            <View style={styles.line}>
-                <Text style={styles.text}>
-                    <Icon
-                        style={styles.icon}
-                        name='warehouse'
-                        type='font-awesome-5' />
-                    Warehouse
-                </Text>
-            </View>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={() => navigation.navigate('ListSuppliersOrders')}>
-            <View style={styles.line}>
-                <Text style={styles.text}>
-                    <Icon 
-                        style={styles.icon}
-                        name='sign-in-alt'
-                        type='font-awesome-5' />
-                    List Supplier's Orders
-                </Text>
-            </View>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={() => navigation.navigate('ListClientsOrders')}>
-            <View style={styles.line}>
-                <Text style={styles.text}>
-                    <Icon 
-                        style={styles.icon}
-                        name='sign-out-alt'
-                        type='font-awesome-5' />
-                    List Client's Orders
-                </Text>
-            </View>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={() => navigation.navigate('ListPickingWaves')}>
-            <View style={styles.line}>
-                <Text style={styles.text}>
-                    <Icon 
-                        style={styles.icon}
-                        name='water'
-                        type='font-awesome-5' />
-                    List Picking Waves
-                </Text>
-            </View>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={() => navigation.navigate('GeneratePickingWaves')}>
-            <View style={styles.line}>
-                <Text style={styles.text}>
-                    <Icon 
-                        style={styles.icon}
-                        name='plus'
-                        type='font-awesome-5' />
-                    Generate Picking Wave
-                </Text>
-            </View>
-        </TouchableHighlight>
-        <View style={styles.logout}>
-            <TouchableHighlight onPress={() => navigation.navigate('Logout')}>
-                <View style={styles.line}>
-                    <Text style={styles.text_logout}>
-                        <Icon 
-                            style={styles.icon}
-                            color='#E5E5E5'
-                            name='door-open'
-                            type='font-awesome-5' />
-                        Logout
-                    </Text>
-                </View>
-            </TouchableHighlight>
-        </View>
-    </View>
-  );
-};
 
 export default function App() {
   return (
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,38 @@
+import React from 'react';
 import { Component } from "react";
+import { StyleSheet, TouchableHighlight, Icon, View, Image } from 'react-native';
 
-class Navbar extends Component{
-    render(){
-        return(
-            <TouchableHighlight onPress={() => props.navigation.toggleDrawer()}>
+import LogoImage from "../images/logo.png"
+
+export default function WarehouseScreen({ navigation }) {
+    return(
+        <View>
+            <TouchableHighlight onPress={() => navigation.toggleDrawer()}>
                 <Icon 
                     style={styles.icon}
-                    name='plus'
+                    color='#E5E5E5'
+                    name='bars'
                     type='font-awesome-5' />
-        </TouchableHighlight>
-        );
-    }
+            </TouchableHighlight>
+            <Image
+                style={styles.image}
+                source={LogoImage}
+            />
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    text: {
+      color: '#171717',
+      fontFamily: 'Corbel',
+      fontStyle: 'normal',
+      fontSize: 18,
+    },
+    image: {
+        width: '60%'
+    },
+    icon:{
+        paddingRight: 10
+    }
+});
