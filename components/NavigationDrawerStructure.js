f1f1fed50764b349de6f8a349d994525ea985c26
leonormsousa <leonormsousa@sapo.pt>
2020-11-26 10:12:23 +0000

    Menu pretty

--- components/NavigationDrawerStructure.js
+++ components/NavigationDrawerStructure.js
@@ -3,64 +3,65 @@ import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 import { Icon } from 'react-native-elements';
 
 const NavigationDrawerStructure = props => {
-
     return (
         <View style={styles.menu}>
-            <TouchableHighlight onPress={() => props.nagivation.navigate('WarehouseScreen')}>
-                <View style={styles.line}>
-                    <Text style={styles.text}>
-                        <Icon
-                            style={styles.icon}
-                            name='warehouse'
-                            type='font-awesome-5' />
-                        Warehouse
-                    </Text>
-                </View>
-            </TouchableHighlight>
-            <TouchableHighlight onPress={() => props.nagivation.navigate('ListSuppliersOrders')}>
-                <View style={styles.line}>
-                    <Text style={styles.text}>
-                        <Icon 
-                            style={styles.icon}
-                            name='sign-in-alt'
-                            type='font-awesome-5' />
-                        List Supplier's Orders
-                    </Text>
-                </View>
-            </TouchableHighlight>
-            <TouchableHighlight onPress={() => props.nagivation.navigate('ListClientsOrders')}>
-                <View style={styles.line}>
-                    <Text style={styles.text}>
-                        <Icon 
-                            style={styles.icon}
-                            name='sign-out-alt'
-                            type='font-awesome-5' />
-                        List Client's Orders
-                    </Text>
-                </View>
-            </TouchableHighlight>
-            <TouchableHighlight onPress={() => props.nagivation.navigate('ListPickingWaves')}>
-                <View style={styles.line}>
-                    <Text style={styles.text}>
-                        <Icon 
-                            style={styles.icon}
-                            name='water'
-                            type='font-awesome-5' />
-                        List Picking Waves
-                    </Text>
-                </View>
-            </TouchableHighlight>
-            <TouchableHighlight onPress={() => props.nagivation.navigate('GeneratePickingWaves')}>
-                <View style={styles.line}>
-                    <Text style={styles.text}>
-                        <Icon 
-                            style={styles.icon}
-                            name='plus'
-                            type='font-awesome-5' />
-                        Generate Picking Wave
-                    </Text>
-                </View>
-            </TouchableHighlight>
+            <View style={styles.list}>
+                <TouchableHighlight onPress={() => props.nagivation.navigate('WarehouseScreen')}>
+                    <View style={styles.line}>
+                        <Text style={styles.text}>
+                            <Icon
+                                style={styles.icon}
+                                name='warehouse'
+                                type='font-awesome-5' />
+                            Warehouse
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+                <TouchableHighlight onPress={() => props.nagivation.navigate('ListSuppliersOrders')}>
+                    <View style={styles.line}>
+                        <Text style={styles.text}>
+                            <Icon 
+                                style={styles.icon}
+                                name='sign-in-alt'
+                                type='font-awesome-5' />
+                            List Supplier's Orders
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+                <TouchableHighlight onPress={() => props.nagivation.navigate('ListClientsOrders')}>
+                    <View style={styles.line}>
+                        <Text style={styles.text}>
+                            <Icon 
+                                style={styles.icon}
+                                name='sign-out-alt'
+                                type='font-awesome-5' />
+                            List Client's Orders
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+                <TouchableHighlight onPress={() => props.nagivation.navigate('ListPickingWaves')}>
+                    <View style={styles.line}>
+                        <Text style={styles.text}>
+                            <Icon 
+                                style={styles.icon}
+                                name='water'
+                                type='font-awesome-5' />
+                            List Picking Waves
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+                <TouchableHighlight onPress={() => props.nagivation.navigate('GeneratePickingWaves')}>
+                    <View style={styles.line}>
+                        <Text style={styles.text}>
+                            <Icon 
+                                style={styles.icon}
+                                name='plus'
+                                type='font-awesome-5' />
+                            Generate Picking Wave
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+            </View>
             <View style={styles.logout}>
                 <TouchableHighlight onPress={() => props.nagivation.navigate('Logout')}>
                     <View style={styles.line}>
@@ -83,7 +84,8 @@ const styles = StyleSheet.create({
     menu:{
         alignItems: 'left',
         justifyContent: 'center',
-        backgroundColor: '#E5E5E5'
+        backgroundColor: '#C4C4C4',
+        flex: 1
     },
     text: {
       color: '#171717',
@@ -92,7 +94,7 @@ const styles = StyleSheet.create({
       fontSize: 18,
     },
     logout:{
-        backgroundColor: '#171717'
+        backgroundColor: '#464646'
     },
     text_logout:{
         color: '#E5E5E5',
@@ -106,6 +108,9 @@ const styles = StyleSheet.create({
     },
     icon:{
         paddingRight: 10
+    },
+    list:{
+        flex: 1,
     }
 });
 
