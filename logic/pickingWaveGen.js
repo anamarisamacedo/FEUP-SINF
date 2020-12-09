71f235e50cf41836a0000e90bcbb66fe419bc558
Sérgio Dias <47751859+SergioDias47@users.noreply.github.com>
2020-12-09 11:54:21 +0000

    Further progress in PW generation

--- logic/pickingWaveGen.js
+++ logic/pickingWaveGen.js
@@ -14,28 +14,55 @@ const functions = {
             }
         }
         orders = orders.slice(0, Math.min(orders.length, Math.max(i + 1, numOrdersToAnalyze)));
+
         let itemsA = {
-            "A11":
+            /*"A11":
                 {
                     qty: 2,
                     items: [{qty: 2, ref: "aq", orderID: "a12"}]
                 }
-            }, itemsB = {}, itemsC = {};
-        let itemsATotal = 0, itemsBTotal = 0, itemsCTotal = 0;
+            */}, itemsB = {}, itemsC = {};
         for(i = 0; i < orders.length; i++) {
             let order = orders[i];
             order.items.forEach(item => {
+                let qtyLeft = item.qty - item.qtyPW;
                 switch(item.loc[0]) {
                     case 'A':
-                        itemsA[item.loc].items.push();
+                        if(item.loc in itemsA) {
+                            itemsA[item.loc].qty += qtyLeft;
+                            itemsA[item.loc].items.push({qty: qtyLeft, ref: item.ref, orderID: order.id});
+                        }
+                        else {
+                            itemsA[item.loc] = {qty: qtyLeft, items: [{qty: qtyLeft, ref: item.ref, orderID: order.id}]};
+                        }
                         break;
                     case 'B':
+                        if(item.loc in itemsB) {
+                            itemsB[item.loc].qty += qtyLeft;
+                            itemsB[item.loc].items.push({qty: qtyLeft, ref: item.ref, orderID: order.id});
+                        }
+                        else {
+                            itemsB[item.loc] = {qty: qtyLeft, items: [{qty: qtyLeft, ref: item.ref, orderID: order.id}]};
+                        }
                         break;
                     case 'C':
+                        if(item.loc in itemsC) {
+                            itemsC[item.loc].qty += qtyLeft;
+                            itemsC[item.loc].items.push({qty: qtyLeft, ref: item.ref, orderID: order.id});
+                        }
+                        else {
+                            itemsC[item.loc] = {qty: qtyLeft, items: [{qty: qtyLeft, ref: item.ref, orderID: order.id}]};
+                        }
                         break;
                 }
-            })
+            });
         }
+
+        console.log(itemsA);
+        console.log(itemsB);
+        console.log(itemsC);
+
+        itemsA.sort((a, b) => (a.qty > b.qty) ? -1 : 1);
 
         // numProducts = 25
         // Order 1 : {Item1 x20  ,  Item2 x40}
